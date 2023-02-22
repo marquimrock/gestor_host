f96095386d23087178635c95274e56fac816ecc3
marquimrock <marquim.ti@gmail.com>
2023-02-22 17:53:25 -0300

    title

--- paginas/home.php
+++ paginas/home.php
@@ -1,7 +1,9 @@
 <div class="page-header">
-    <h3 align="center">Gerenciador de Pedidos</h3>
+    <h1 align="center"></h1>
 </div>
-
+<div class="panel panel-primary">
+            <div class="panel-heading" align="center" style="font-size: 20px;">Gerenciador de Pedidos</div>         
+        </div> 
 <p>
    
 </p>
